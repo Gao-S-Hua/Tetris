e7aeb9e1d6d57c30c8bb5dcea3cb06f9a30f5db8
Shaohua Gao <shaohuag@xlnx.xilinx.com>
2019-11-08 16:23:17 +0800

    fix webpack bug

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Head from './component/head/';
 import {Provider} from 'react-redux';
 import Loadable from 'react-loadable';
 import store from './store';
-// import {Icon} from 'antd';
+import {Icon} from 'antd';
 
-// const loading = <Icon type="loading" style={{ fontSize: '300px', margin : '100px 100px'}}/>;
-const loading = <div>Loading ...</div>
+const loading = <Icon type="loading" style={{ fontSize: '300px', margin : '100px 100px'}}/>;
+// const loading = <div>Loading ...</div>
 const Body = Loadable({
     loader : () => (import('./component/body/index')),
     loading : () => loading
